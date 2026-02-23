7ba0dc4d616e8d1cdb4132743fee0acc1b4a189b
Ankur <ankurkumark2024@gmail.com>
2026-02-23 15:14:44 +0530

    Leverage Vercel Native API Catch-All Routing

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,26 +1,6 @@
-import express from "express";
-import { registerRoutes } from "../server/routes";
-import { createServer } from "http";
-
-const app = express();
-const httpServer = createServer(app);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-let initialized = false;
-let initPromise: Promise<any> | null = null;
+import { vercelHandler } from "../server/index";
 
 export default async function handler(req: any, res: any) {
-    if (!initialized) {
-        if (!initPromise) {
-            initPromise = registerRoutes(httpServer, app).then(() => {
-                initialized = true;
-            }).catch(console.error);
-        }
-        await initPromise;
-    }
-
-    // Hand off the request to the fully setup Express app
-    return app(req, res);
+    // Pass the serverless request to our fully initialized Express app
+    return await vercelHandler(req, res);
 }
--- api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("../dist/index.cjs");
-
-module.exports = async function handler(req, res) {
-    // Pass the request to the compiled Express app handler
-    return await app.vercelHandler(req, res);
-};
