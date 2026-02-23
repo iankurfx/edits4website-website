96935abbcfb2ec5cf482fc34c57d81892d9e8ba6
Ankur <ankurkumark2024@gmail.com>
2026-02-23 22:10:32 +0530

    Remove conflicting api route file that caused Vercel build conflict

--- api/[...path].ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-import vercelApp from "../dist/index.cjs";
-export default async function handler(req: any, res: any) {
-    if (vercelApp.vercelHandler) return await vercelApp.vercelHandler(req, res);
-    if (vercelApp.default) return await vercelApp.default(req, res);
-    return res.status(500).json({ error: "Failed to load Vercel App bundle" });
-}
