a2e64154c75c96b0ae2b488322173d40719fdd29
Ankur <ankurkumark2024@gmail.com>
2026-02-23 21:58:01 +0530

    Configure Vercel Serverless to wrap bundled dist correctly

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,5 +1,7 @@
-import { vercelHandler } from "../server/index.js";
+// @ts-ignore
+import vercelApp from "../dist/index.cjs";
 export default async function handler(req: any, res: any) {
-    // Pass the serverless request to our fully initialized Express app
-    return await vercelHandler(req, res);
+    if (vercelApp.vercelHandler) return await vercelApp.vercelHandler(req, res);
+    if (vercelApp.default) return await vercelApp.default(req, res);
+    return res.status(500).json({ error: "Failed to load Vercel App bundle" });
 }
