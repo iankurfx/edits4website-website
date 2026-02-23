d90ddcf01a54e677dc48e7eaceb399c2155642a8
Ankur <ankurkumark2024@gmail.com>
2026-02-23 14:17:27 +0530

    Moved Firebase config to environment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCo4NijcOYvQoCI1ENqSFIlZIJOx13Chjo",
-    authDomain: "edit4baddies.firebaseapp.com",
-    projectId: "edit4baddies",
-    storageBucket: "edit4baddies.firebasestorage.app",
-    messagingSenderId: "193088440608",
-    appId: "1:193088440608:web:fa35fa01989bd79ff96f45",
-    measurementId: "G-BX3QR8LDGM"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
